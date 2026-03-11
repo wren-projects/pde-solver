883f0c40f5fce11dc187c2535b1634a102cf6110
Maneren <maneren731@gmail.com>
2026-03-11 13:28:50 +0100

    feat: create module interfaces

---
+++ src/pde_solver/abc/boundary.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+from pde_solver.types import DType, Vector
+
+
+class BoundaryCondition(ABC):
+    """Interface for boundary conditions."""
+
+    @abstractmethod
+    def __call__(self, position: Vector, time: float) -> DType:
+        """Return the boundary values in given point at given time."""
+        raise NotImplementedError
---
+++ tests/solver_tester.py
@@ -0,0 +1,13 @@
+from pde_solver.abc.solver import Solver
+
+
+class SolverTester:
+    """Class for testing solvers."""
+
+    def __init__(self, solver: Solver) -> None:
+        """Initialize the solver tester with given solver."""
+        self.solver: Solver = solver
+
+    """
+    TODO: actually implement this.
+    """
