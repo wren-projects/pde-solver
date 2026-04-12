155c33712418b726c7c03d4f4e8db81da5f1b820
Maneren <maneren731@gmail.com>
2026-04-12 15:06:39 +0200

    feat(ttd): implement scalar multiplication
    By extension, also implements negation.

---
+++ src/numpy_ttd/ops.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from math import prod
+from typing import TYPE_CHECKING
+
+import numpy as np
+
+if TYPE_CHECKING:
+    from numpy_ttd.ttd import TTD
+
+
+def scalar_mul[DType: np.floating](
+    a: TTD[DType],
+    b: np.floating | float,
+    out: tuple[TTD[DType]] | None = None,
+) -> TTD[DType]:
+    """Multiply a TTD object by a scalar."""
+    cores = out[0].data if out is not None else a.data.copy()
+
+    index, _ = min(enumerate(cores), key=lambda el: prod(el[1].shape))
+
+    cores[index] = np.multiply(cores[index], b)
+
+    return a.__class__(cores, dtype=a.dtype)
+
+
+def neg[DType: np.floating](
+    a: TTD[DType],
+) -> TTD[DType]:
+    """Negate a TTD object."""
+    return scalar_mul(a, -1.0)
