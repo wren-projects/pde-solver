0348766efb217d1372e727a54d8fc264d002046e
Maneren <maneren731@gmail.com>
2026-04-16 16:21:47 +0200

    simplify and optimize addition

--- src/numpy_ttd/ops.py
+++ src/numpy_ttd/ops.py
@@ -5,12 +5,9 @@ from typing import TYPE_CHECKING
 
 import numpy as np
 
-from numpy_ttd.math import DEFAULT_EPSILON
-from numpy_ttd.types import Core, Matrix
+from numpy_ttd.types import Core
 
 if TYPE_CHECKING:
-    from numpy.typing import NDArray
-
     from numpy_ttd.ttd import TTD
 
 
@@ -30,7 +27,7 @@ def add[DType: np.floating](
     if out is not None and out[0].shape != a.shape:
         raise ValueError("Output tensor has an incorrect shape.")
 
-    cores = _add_cores(a.data, b.data, a.shape, a.dtype)
+    cores = _add_cores(a.data, b.data, a.dtype)
 
     if out is None:
         return TTD(cores, dtype=a.dtype)
@@ -42,36 +39,28 @@ def add[DType: np.floating](
 def _add_cores[DType: np.floating](
     a: list[Core[DType]],
     b: list[Core[DType]],
-    ranks: tuple[int, ...],
     dtype: np.dtype,
 ) -> Iterable[Core[DType]]:
-
     # Add vectors
     if len(a) == len(b) == 1:
         return [np.add(a[0], b[0])]
 
-    def create_block(matrix_a: Matrix[DType], matrix_b: Matrix[DType]) -> Matrix[DType]:
-        # first core
-        if matrix_a.shape[0] == 1:
-            return np.hstack((matrix_a, matrix_b))
+    def merge_cores(core_a: Core[DType], core_b: Core[DType]) -> Core[DType]:
+        am, rank, an = core_a.shape
+        bm, _, bn = core_b.shape
 
-        # last core
-        if matrix_a.shape[1] == 1:
-            return np.vstack((matrix_a, matrix_b))
-
-        # middle cores
-        am, an = matrix_a.shape
-        bm, bn = matrix_b.shape
-
-        result = np.zeros((am + bm, an + bn), dtype=dtype)
-        result[:am, :an] = matrix_a
-        result[am:, an:] = matrix_b
+        result = np.zeros((am + bm, rank, an + bn), dtype=dtype)
+        # upper left block
+        result[:am, :, :an] = core_a
+        # lower right block
+        result[am:, :, an:] = core_b
         return result
 
-    def merge_cores(core_a: Core[DType], core_b: Core[DType], rank: int) -> Core[DType]:
-        return np.stack(
-            [create_block(core_a[:, i, :], core_b[:, i, :]) for i in range(rank)],
-            axis=1,
-        )
-
-    return map(merge_cores, a, b, ranks)
+    return [
+        # stack first cores horizontally
+        np.concatenate((a[0], b[0]), axis=2),
+        # merge middle cores into blocks
+        *map(merge_cores, a[1:-1], b[1:-1]),
+        # stack last cores vertically
+        np.concatenate((a[-1], b[-1]), axis=0),
+    ]
