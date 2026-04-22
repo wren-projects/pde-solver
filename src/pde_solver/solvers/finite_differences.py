d3dc470b5a16fda9fc23c45cecaf793e40f1cc63
gavlna <kopciljakub@gmail.com>
2026-04-22 16:34:43 +0200

    Feat: Finite differences solver reverted to a usable state, yet not implemented

--- src/pde_solver/solvers/finite_differences.py
+++ src/pde_solver/solvers/finite_differences.py
@@ -1,10 +1,8 @@
-from math import ceil
 from typing import final, override
 
-from pde_solver.abc.boundary import BoundaryCondition
 from pde_solver.abc.pde import PDE
 from pde_solver.abc.solver import Solver
-from pde_solver.pde_types import DType, NDArray
+from pde_solver.pde_types import DType, NDArray, Vector
 
 
 @final
@@ -30,35 +28,11 @@ class FiniteDifferences(Solver):
         self.timestep = timestep
 
     @override
-    def __call__(
+    def _get_time_step(
         self,
         pde: PDE,
-        initial_condition: NDArray,
-        discretization_step: DType,
-        boundary_condition: BoundaryCondition,
-        time: DType,
+        state: NDArray,
+        spacial_discretization_step: Vector,
+        time_discretization_step: DType,
     ) -> NDArray:
-        """Compute the state at the given time given the PDE and it's conditions."""
-        r = self.timestep / discretization_step**2
-
-        if r > self.STABILITY_TRESHOLD:
-            raise ValueError("Timestep too big")
-
-        dim = initial_condition.ndim
-
-        state = initial_condition
-
-        state = boundary_condition(state, 0.0)  # enforce boundary_condition at time = 0
-
-        # look for the class of the PDE and set parameters True/False accordingly
-        # So it knows if it needs time_derivative, second_order space_deriv, and first order space_deri
-        for step in range(ceil(time / self.timestep)):
-            current_time = step * self.timestep
-
-            new_state = state.copy()
-            for i in range(dim):
-                # update state
-                pass
-            state = new_state
-
-        return state
+        raise NotImplementedError
