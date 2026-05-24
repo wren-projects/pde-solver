236ff003acd04813798bdb3028aca1fc2f8d0ac9
gavlna <kopciljakub@gmail.com>
2026-05-24 23:24:00 +0200

    Feat: Operators added

---
+++ src/pde_solver/operators.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+from pde_solver.pde_types import NDArray, Vector
+
+
+class Gradient:
+    """Gradient operator for NDArray."""
+
+    def __call__(self, tensor: NDArray, spacial_step: Vector) -> NDArray:
+        """
+        Compute the gradient of the tensor with given spacial_steps.
+
+        Spacial_step length must match the number of dimensions of tensor.
+
+        The output is one dimension bigger with the first dimension corresponding
+        to the dimension along which derivative is taken.
+        """
+        return np.gradient(tensor, spacial_step)
+
+
+class Laplace:
+    """Laplace operator for NDArray."""
+
+    def __call__(self, tensor: NDArray, spacial_step: Vector) -> NDArray:
+        """
+        Compute the Laplace transform of the tensor with given spacial_steps.
+
+        Spacial_step length must match the number of dimensions of tensor.
+
+        The output is the same dimension as tensor.
+        """
+        return divergence(gradient(tensor, spacial_step), spacial_step)
+
+
+class Divergence:
+    """Divergence operator for NDArray."""
+
+    def __call__(self, tensor: NDArray, spacial_step: Vector) -> NDArray:
+        """
+        Compute the divergence of the tensor with given spacial_steps.
+
+        Spacial_step length must be one smaller than the number of dimensions of tensor.
+
+        The output is one dimension smaller than tensor.
+        First dimension of tensor is contracted with the nabla.
+        """
+
+
+gradient = Gradient()
+laplace = Laplace()
+divergence = Divergence()
