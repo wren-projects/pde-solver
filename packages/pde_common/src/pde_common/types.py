bfde5f7dd6130b16e0dae99f692f307dfa5bdafe
Maneren <maneren731@gmail.com>
2026-05-28 17:39:44 +0200

    feat(ttd): implement addition broadcasting

--- packages/pde_common/src/pde_common/types.py
+++ packages/pde_common/src/pde_common/types.py
@@ -6,5 +6,7 @@ type Vector[T: np.floating] = np.ndarray[tuple[int], np.dtype[T]]
 type Matrix[T: np.floating] = np.ndarray[tuple[int, int], np.dtype[T]]
 type NDArray[T: np.number] = np.ndarray[tuple[int, ...], np.dtype[T]]
 
+type Scalar = np.floating | float | int
+ScalarTypes = (np.number, float, int)
 
 type Index1D = SupportsIndex | slice[SupportsIndex | None]
