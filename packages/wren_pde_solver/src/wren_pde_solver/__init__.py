6daa81dd34c8ff0726a9a4b90904d5d0324ffa0e
gavlna <kopciljakub@gmail.com>
2026-06-16 22:11:50 +0200

    some fixes

--- packages/wren_pde_solver/src/wren_pde_solver/__init__.py
+++ packages/wren_pde_solver/src/wren_pde_solver/__init__.py
@@ -1,5 +1,5 @@
-from wren_pde_solver import pde as PDES
+from wren_pde_solver import pde as ALL_PDES  # noqa: N812 -- allow capital case
 from wren_pde_solver.boundary_conditions import ConstantDirichletBoundaryCondition
 from wren_pde_solver.solvers.finite_differences import FiniteDifferences
 
-__all__ = ["PDES", "ConstantDirichletBoundaryCondition", "FiniteDifferences"]
+__all__ = ["ALL_PDES", "ConstantDirichletBoundaryCondition", "FiniteDifferences"]
