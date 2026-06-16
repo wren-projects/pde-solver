33d47f4620dfa1685ac6dfd7ad86822aa7bd4ebe
gavlna <kopciljakub@gmail.com>
2026-06-16 13:30:26 +0200

    prepared for deploy
    a

--- packages/pde_ttd/src/pde_ttd/__init__.py
+++
@@ -1,4 +0,0 @@
-from pde_ttd.core import TTD
-from pde_ttd.math import DEFAULT_EPSILON
-
-__all__ = ["DEFAULT_EPSILON", "TTD"]
---
+++ packages/wren_ttd/src/wren_ttd/__init__.py
@@ -0,0 +1,4 @@
+from wren_ttd.core import TTD
+from wren_ttd.math import DEFAULT_EPSILON
+
+__all__ = ["DEFAULT_EPSILON", "TTD"]
