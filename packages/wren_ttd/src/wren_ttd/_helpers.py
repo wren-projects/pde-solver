feca8b7a4f0c52a25e668a5e2f0da5dad2e7bec0
Maneren <maneren731@gmail.com>
2026-08-28 20:03:01 +0200

    rename and add docstring

--- packages/wren_ttd/src/wren_ttd/_helpers.py
+++ packages/wren_ttd/src/wren_ttd/_helpers.py
@@ -12,8 +12,29 @@ def reverse_cores[DType: np.floating](
     return (core.T for core in reversed(cores))
 
 
-def to_int_tuple(axes: int | Iterable[int]) -> tuple[int, ...]:
-    return (int(axes),) if isinstance(axes, int) else tuple(map(int, axes))
+def to_int_tuple(a: int | Iterable[int]) -> tuple[int, ...]:
+    """
+    Convert an int or iterable of ints to a tuple of ints.
+
+    If the input is an int, it is converted to a tuple of length 1. An iterable
+    is converted to a tuple of the same length. Useful for normalizing
+    NumPy-style shape or axis arguments to single type.
+
+    See also: :func:`numpy.core.multiarray.normalize_axis_tuple` if the handling
+    of negative and out-of-bounds indices is desired.
+
+    Parameters
+    ----------
+    a : int | Iterable[int]
+        The input to convert.
+
+    Returns
+    -------
+    tuple[int, ...]
+        The converted tuple.
+
+    """
+    return (int(a),) if isinstance(a, int) else tuple(map(int, a))
 
 
 def orthogonalize_right[DType: np.floating](cores: list[Core[DType]]) -> None:
